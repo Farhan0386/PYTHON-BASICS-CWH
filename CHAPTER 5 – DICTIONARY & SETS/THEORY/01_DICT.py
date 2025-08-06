33a78c08daa0e65d5f6a7db5ca5296a8ca1c96ac
Farhan Hussain <farhan03860@gmail.com>
2025-08-06 18:56:56 +0530

    Add files via upload

---
+++ CHAPTER 5 – DICTIONARY & SETS/THEORY/01_DICT.py
@@ -0,0 +1,8 @@
+d = {} # Empty dictionary
+marks = {
+    "Farhan": 100,
+    "Sahil": 56,
+    "Rihan": 23
+}
+# print(marks, type(marks))
+print(marks["Farhan"]) # Accessing value using key
---
+++ CHAPTER 5 – DICTIONARY & SETS/THEORY/05_SET_UNION_INTERSECTION.py
@@ -0,0 +1,6 @@
+
+s1 = {1,3,4,5,6}  # Set with unique elements
+s2 = {5,6,7,8,9} # Another set with unique elements
+
+print(s1.union(s2)) # Union of two sets
+print(s1.intersection(s2)) # Intersection of two sets
