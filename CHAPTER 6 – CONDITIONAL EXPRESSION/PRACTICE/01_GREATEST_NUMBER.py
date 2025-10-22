156be24753e52cda13f39403ececdec9cb044272
Farhan Hussain <farhan03860@gmail.com>
2025-10-22 09:39:04 +0530

    conditional statement

---
+++ CHAPTER 6 – CONDITIONAL EXPRESSION/PRACTICE/01_GREATEST_NUMBER.py
@@ -0,0 +1,6 @@
+a=int(input("Enter your first number: "))
+b=int(input("Enter your second number: "))
+c=int(input("Enter your third number: "))
+d=int(input("Enter your fourth number: "))
+
+if a
---
+++ CHAPTER 6 – CONDITIONAL EXPRESSION/THEORY/02_if_elif_else_ladder.py
@@ -0,0 +1,11 @@
+age=int(input("Enter your age :"))
+if age>=18:  # If elif else ladder
+    print("You are now  adult . you can drive a car as well as ride a two wheeler")
+elif age>=16:
+    print("you can only ride a two wheeler")
+elif age==0|age>0:
+    print(" 0 is Invalid age , Please enter a valid age")
+else:
+    print("Invalid age , Please enter a valid age")
+print()
+print("End of Program")
