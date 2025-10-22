698df30d9c22433adeee164522cfaff21632c94f
Farhan Hussain <farhan03860@gmail.com>
2025-10-22 10:41:56 +0530

    practice question  CH 6

--- CHAPTER 6 – CONDITIONAL EXPRESSION/PRACTICE/01_GREATEST_NUMBER.py
+++ CHAPTER 6 – CONDITIONAL EXPRESSION/PRACTICE/01_GREATEST_NUMBER.py
@@ -2,4 +2,14 @@ a=int(input("Enter your first number: "))
 b=int(input("Enter your second number: "))
 c=int(input("Enter your third number: "))
 d=int(input("Enter your fourth number: "))
-
+print()
+if a>b and a>c and a>d:
+    print(f"The greatest number is first : {a}")
+elif b>c and b>d:
+    print(f"The greatest number is second : {b}")
+elif c>d:
+    print(f"The greatest number is third :{c}")
+else:
+    print(f"The gratest number is fourth : {d}")
+print()
+print("End of program")
